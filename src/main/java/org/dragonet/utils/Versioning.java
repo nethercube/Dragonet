50f05bc1c08bd5c3e92cf66ae539e99f7c470fb4
lolcraftboy14 <lolcraftboy13.gamer@gmail.com>
2016-06-01 10:32:01 +0200

    14.3 (;

--- src/main/java/org/dragonet/utils/Versioning.java
+++ src/main/java/org/dragonet/utils/Versioning.java
@@ -20,7 +20,7 @@ public class Versioning {
 	
 	public final static String DRAGONET_VERSION = "0.0.1";
 	
-	public final static String MCPE_VERSION = "0.14.2";
-	public final static int MCPE_PROTOCOL = 60;
+	public final static String MCPE_VERSION = "0.14.3";
+	public final static int MCPE_PROTOCOL = 70;
 	
 }
